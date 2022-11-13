e260a9e20b9f5cf11f8e4f76fce4db5a37b7be7a
hannmx <mxkud@mail.ru>
2022-11-13 13:49:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGB_HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое значение N больше 0: ");
+            int N = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= N)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+                i++;
+            }
+        }
+    }
+}
